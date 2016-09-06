f6ae628b3b5c182d9eaf53fd240ae56d9857d65d
Nhan Cao <nhancv1992@gmail.com>
2016-09-06 13:55:04 +0700

    initial

---
+++ app/src/main/java/com/nhancv/cardreader/MainActivity.java
@@ -0,0 +1,100 @@
+package com.nhancv.cardreader;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int SCAN_REQUEST_CODE = 0x100;
+
+    private Button btScan;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btScan = (Button) findViewById(R.id.btScan);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        tvResult.setText("card.io library version: " + CardIOActivity.sdkVersion() + "\nBuilt: " + CardIOActivity.sdkBuildDate());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (CardIOActivity.canReadCardWithCamera()) {
+            btScan.setText("Scan a credit card with card.io");
+        } else {
+            btScan.setText("Enter credit card information");
+        }
+    }
+
+    public void btScanOnClick(View v) {
+        // This method is set up as an onClick handler in the layout xml
+        // e.g. android:onClick="onScanPress"
+
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
+
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_RESTRICT_POSTAL_CODE_TO_NUMERIC_ONLY, false); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CARDHOLDER_NAME, false); // default: false
+
+        // hides the manual entry button
+        // if set, developers should provide their own manual entry mechanism in the app
+        scanIntent.putExtra(CardIOActivity.EXTRA_SUPPRESS_MANUAL_ENTRY, false); // default: false
+
+        // matches the theme of your application
+        scanIntent.putExtra(CardIOActivity.EXTRA_KEEP_APPLICATION_THEME, false); // default: false
+
+        // SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, SCAN_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String resultStr;
+        if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+            CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+            // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+            resultStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+
+            // Do something with the raw number, e.g.:
+            // myService.setCardNumber( scanResult.cardNumber );
+
+            if (scanResult.isExpiryValid()) {
+                resultStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+            }
+
+            if (scanResult.cvv != null) {
+                // Never log or display a CVV
+                resultStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+            }
+
+            if (scanResult.postalCode != null) {
+                resultStr += "Postal Code: " + scanResult.postalCode + "\n";
+            }
+
+            if (scanResult.cardholderName != null) {
+                resultStr += "Cardholder Name : " + scanResult.cardholderName + "\n";
+            }
+        } else {
+            resultStr = "Scan was canceled.";
+        }
+        tvResult.setText(resultStr);
+
+    }
+}
